6ebcaa19e1bf0e5b24803452ed7d2eb175c43647
jdsooo <jdsaquiqu@gmail.com>
2024-05-22 20:54:21 -0600

    v1 footer

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,13 @@
-let fjm=document.querySelector(".footer");
-fjm.innerHTML=`
-<p>scl.edu.gt</p>
+let li_jsaquique=document.querySelector(".footer-content");
+ 
+li_jsaquique.innerHTML=`
 
-`;
+            <p>&copy;Todos los derechos reservados.</p>
+            <div class="social-links">
+                <a href="#"><img src="https://z-m-static.xx.fbcdn.net/rsrc.php/v3/yD/r/5D8s-GsHJlJ.png" alt="Facebook"></a>
+                <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png?20220821125553" alt="Twitter"></a>
+                <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png" alt="Instagram"></a>
+                <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/1200px-GitHub_Invertocat_Logo.svg.png" alt="GitHub"></a>
+            </div>
+
+ `;
